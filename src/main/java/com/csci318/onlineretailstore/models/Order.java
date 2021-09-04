ae9e24ba7949cab2b5b4c83df464ed65c91bf5f5
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-04 11:50:49 +1000

    Created POJO for the Order class

--- src/main/java/com/csci318/onlineretailstore/models/Order.java
+++ src/main/java/com/csci318/onlineretailstore/models/Order.java
@@ -3,6 +3,7 @@ package com.csci318.onlineretailstore.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Order {
@@ -14,5 +15,73 @@ public class Order {
     private String product;
     private String quantity;
 
+    public Order() {
+    }
 
+    public Order(String supplier, String product, String quantity) {
+        this.supplier = supplier;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", supplier='" + supplier + '\'' +
+                ", product='" + product + '\'' +
+                ", quantity='" + quantity + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+        if (!(o instanceof Order))
+            return false;
+        Order Order = (Order) o;
+        return Objects.equals(this.id, Order.id)
+                && Objects.equals(this.supplier, Order.supplier)
+                && Objects.equals(this.product, Order.product)
+                && Objects.equals(this.quantity, Order.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.supplier, this.product, this.quantity);
+    }
 }
