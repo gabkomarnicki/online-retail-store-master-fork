941cfcd7d1b90c2c3b7cdc6ae6d375cc8e9cc7ef
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-11 11:18:15 +1000

    Added Validator

--- src/main/java/com/csci318/onlineretailstore/service/OrderService.java
+++ src/main/java/com/csci318/onlineretailstore/service/OrderService.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import javax.xml.validation.Validator;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private Validator validator;
 
     @Autowired
     public OrderService(OrderRepository orderRepository) {
