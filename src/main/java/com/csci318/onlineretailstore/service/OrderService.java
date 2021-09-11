48864e2b731f1fb34a7823814b328425197eeec1
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-11 10:54:15 +1000

    Addee @Autowired annotation

--- src/main/java/com/csci318/onlineretailstore/service/OrderService.java
+++ src/main/java/com/csci318/onlineretailstore/service/OrderService.java
@@ -2,6 +2,7 @@ package com.csci318.onlineretailstore.service;
 
 import com.csci318.onlineretailstore.models.Order;
 import com.csci318.onlineretailstore.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    @Autowired
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
